02b91714bf9f24d10fbe08736846a59a6c346823
dewchat <chatchada.d13@gmail.com>
2025-02-15 16:55:49 +0700

    test

--- src/components/Part13.js
+++ src/components/Part13.js
@@ -11,7 +11,7 @@ const Part13 = () => {
     setAnswers((prevAnswers) => ({
       ...prevAnswers,
       [name]: {
-        ...prevAnswers[name],
+        ...prevAnswers[name], 
         [value]: checked,
       },
     }));
